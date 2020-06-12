063f8f2365287d83e5e98d41b062e3cc4eb92361
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-06-12 23:26:02 +0300

    add collapse menu and burger button

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
         $('.active_tab').removeClass('active_tab');
         $(this).toggleClass('active_tab');
     })
+
+    // collapse menu rotate
+    $('.burger_menu').click(function(){
+        $(this).toggleClass('burger_active');
+        $('.collapse_menu').toggleClass('collapse_menu_active');
+    })
 })
